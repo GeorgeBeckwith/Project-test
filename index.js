3e468f8c7b5a34c82ccf41f4db9875722bd3c641
GeorgeBeckwith <george.jbeckwith@googlemail.com>
2020-03-13 16:47:22 +0000

    Http-server u

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var express = require('express')
+var path = require('path')
+var request = require('request')
+
 var app = express()
 var server = require('http').Server(app)
-var port = process.env.PORT || 3000;
+ 
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
+var port = process.env.PORT || 3000;
 server.listen(port, error => {
     if (error)
     {
         console.log('Server error: ' + error);
     }
-    console.log('Server started! Visit http://localhost:' + port);
+
+    console.log('Server started. Visit http://localhost:' + port);
 })
+
+//request('www.http://watchout4snakes.com/wo4snakes/Random/RandomWord').pipe(res)
