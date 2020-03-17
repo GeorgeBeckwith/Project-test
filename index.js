d32fffb93011d41f4c648850123071756a265494
GeorgeBeckwith <george.jbeckwith@googlemail.com>
2020-03-17 00:15:34 +0000

    Value entered is stored in the data base.

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-//Server fot static files
-const express = require("express");
-const app = express();
+//Server for static files
+const express = require("express"); //Imports express module
+const app = express(); //Stores link to express module in app variable
 
-var port = 3000
-app.listen(port, () => console.log("Listening at", port));
-app.use(express.static("public"));
+var port = 3000 //Assigns the value of 3000 to the variable port
+app.listen(port, () => {
+    console.log("Listening at", port); //When server is started shows which port is being used
+});
+app.use(express.static("public")); //This gives access to the public file where the static files are stored
 
+//Once received data from the user its stored in the database
+const dataStore = require("nedb") //Imports the ability to access the use of a database through nedb
+const logInDB = new dataStore("logInDB")
+logInDB.loadDatabase();
 
-//Allows for data to be sent from clients when submitting details which will be stored in separate server
+//Allows for data to be sent from clients when submitting details which will be stored in a separate server
 app.use(express.json());
 app.post("/data", (request, response) => {
     console.log(request.body); 
+    const data = request.body;
+    logInDB.insert(data)
     response.end()
 });
--- public/js/main.js
+++ public/js/main.js
@@ -2,13 +2,14 @@ function sendData() {
     var name = document.getElementById("name").value;
     var surname = document.getElementById("surname").value;
 
-    const inputs = { name, surname};
-    const options = {
-        method: "POST",
+    const inputs = { name, surname}; //Adds both inputs into the same variable
+    const options = { //Defines what method will be carried out on the data
+        method: "POST", //This declares that the data will be sent
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json" //Declares format of the data
         },
-        body: JSON.stringify(inputs)
+        body: JSON.stringify(inputs) //Applies JSON format to the data
     }
     fetch("/data", options);
 };
+
